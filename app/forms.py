c7ac28161acdb02f9982a6758d07fd0580208e3a
cintiabass <mais_1_cintia@hotmail.com>
2021-09-25 14:09:16 -0300

    Front

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from app.models import Empresa, Produto
+
+# Create the form class.
+class EmpresaForm(ModelForm):
+  class Meta:
+    model = Empresa
+    fields = ['nome', 'razao', 'cnpj', 'telefone', 'segmento']
+
+class ProdutoForm(ModelForm):
+  class Meta:
+    model = Produto
+    fields = ['nomep', 'tipo', 'preco', 'qtdade', 'fornecedor', 'empresa']
+    
