4018494bd195702b1b8e0d4916c7075198c70f74
Eri-93 <ericappimentel@gmail.com>
2021-09-27 13:14:13 -0300

    Estlização do front

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Rayan\Desktop\Cloud9\cloud-magalu\venv
+set VIRTUAL_ENV=C:\LuizaCode\ProjetoMagalu\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
